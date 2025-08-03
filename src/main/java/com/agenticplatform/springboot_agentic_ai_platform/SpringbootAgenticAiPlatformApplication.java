66f4aa21a255bd26246f7d85e2be773c17482b81
ChayanMakwana <chayanmak7@gmail.com>
2025-08-03 22:21:36 +0530

    Initial Commit: Spring Boot Agentic AI Platform

---
+++ src/main/java/com/agenticplatform/springboot_agentic_ai_platform/SpringbootAgenticAiPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.agenticplatform.springboot_agentic_ai_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAgenticAiPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAgenticAiPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agenticplatform/springboot_agentic_ai_platform/SpringbootAgenticAiPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agenticplatform.springboot_agentic_ai_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAgenticAiPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
